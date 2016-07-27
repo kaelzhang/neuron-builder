a0dc4f6bffa32dbc79b824294e62766652206b07
Kael <i@kael.me>
2016-07-28 02:12:22 +0800

    test cases; undone

--- test/fixtures/nodes.js
+++ test/fixtures/nodes.js
@@ -1,10 +1,38 @@
 'use strict'
 
 const clone = require('clone')
+const babylon = require('babylon')
+
+const {
+  SourceNode
+} = require('source-map')
+
+const CODE_A = `require('./b')
+require('b')
+`
+
+const FILE_B = '/path/to/b.json'
+
+const CODE_B = `{
+  "a": 1,
+  "b": []
+}
+`
+
+const code_b_modularized = (function(){
+  let node = new SourceNode(1, 0, FILE_B, CODE_B)
+  node.prepend('module.exports = ')
+  return node.toStringWithSourceMap({
+    file: FILE_B
+  })
+})()
+
 
 const NODES = {
   '/path/to/a.js': {
     id: '/path/to/a.js',
+    code: CODE_A,
+    ast: babylon.parse(CODE_A),
     require: {
       './b': '/path/to/b.json',
       'b': 'b'
@@ -14,7 +42,10 @@ const NODES = {
   },
 
   '/path/to/b.json': {
-    id: '/path/to/b.json',
+    id: FILE_B,
+    code: code_b_modularized.code,
+    ast: babylon.parse(code_b_modularized.code),
+    map: code_b_modularized.map,
     type: ['require'],
     json: true
   },
